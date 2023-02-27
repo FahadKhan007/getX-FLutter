58d2cd565c01199c536d198b813d74efbfafbe25
FahadKhan007 <fahad.bauet@gmail.com>
2023-02-27 17:05:32 +0600

    Show Dialog on press and bottom to top snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       title: "Learn getX",
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.greenAccent,
           title: const Text("Hey this is Fahad learing getX"),
         ),
         body: Center(
@@ -23,10 +24,50 @@ class MyApp extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.greenAccent),
+                ),
                 onPressed: () {
-                  Get.snackbar("Hey this is getX", "I am getX learner");
+                  Get.snackbar(
+                    "Hey this is getX",
+                    "I am getX learner",
+                    colorText: Colors.greenAccent,
+                    backgroundColor: Colors.blueGrey,
+                    snackPosition: SnackPosition.BOTTOM,
+                    dismissDirection: DismissDirection.vertical,
+                    duration: const Duration(
+                      seconds: 1,
+                    ),
+                  );
                 },
                 child: const Text("Hey show getX"),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.greenAccent),
+                ),
+                onPressed: () {
+                  Get.defaultDialog(
+                    title: "Hey we are learning getX",
+                    titleStyle: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                    middleText: "Hey this is middle property",
+                    middleTextStyle: const TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
+                    ),
+                    backgroundColor: Colors.greenAccent,
+                    radius: 70,
+                  );
+                },
+                child: const Text("Hey show dialog box"),
               ),
             ],
           ),
