54084a6872bae13e5b69b4ee3009707105f18c59
FahadKhan007 <fahad.bauet@gmail.com>
2023-02-28 16:33:01 +0600

    Reactive custom class state management by getX

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx/customclass.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  var count = 0.obs;
-  void increaseNum() {
-    count++;
-  }
+  var learner = CustomClass();
+
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Hey we are learning state management using getX",
+      title: "We are learning getX obx with custom class",
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.redAccent,
-          title: const Text("Counting Pages"),
+          backgroundColor: Colors.greenAccent,
+          title: const Text(
+            "State Management obx with custom class",
+            style: TextStyle(
+              color: Colors.black87,
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2,
+            ),
+          ),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Obx(
-                () => Text(
-                  "Increase Number: $count",
-                  style: const TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
+        body: Container(
+          color: Color.fromARGB(255, 82, 74, 74),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Obx(
+                  () => Text(
+                    "Name ${learner.name.value}",
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                        color: Colors.white70),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.redAccent),
+                const SizedBox(
+                  height: 30,
                 ),
-                onPressed: () {
-                  increaseNum();
-                },
-                child: const Text("Increse Number"),
-              ),
-            ],
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.redAccent),
+                  ),
+                  onPressed: () {
+                    learner.name.value = learner.name.value.toUpperCase();
+                  },
+                  child: const Text("Make it to UPPER CASE"),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.blueAccent),
+                  ),
+                  onPressed: () {
+                    learner.name.value = learner.name.value.toLowerCase();
+                  },
+                  child: const Text("Make it to lower case"),
+                ),
+              ],
+            ),
           ),
         ),
       ),
